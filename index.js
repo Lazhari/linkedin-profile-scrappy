ff3d31ab3cb49442fc04d7a301c44750e858ce24
Lazhari <lazhari.mohammed@outlook.com>
2015-12-14 15:16:38 +0000

    the server proxy

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/scrape', function(req, res){
 	  url: url,
 	  headers: {
 		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
-	  }
+	  },
+	  proxy: "72.240.34.12"
 	};
     var profile = {
         name:"",
